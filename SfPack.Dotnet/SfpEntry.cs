3649d5d9bbb479ba840587123eba0c0eedd3942d
josephmoresena <44370115+josephmoresena@users.noreply.github.com>
2019-04-10 18:09:56 -0500

    DotNet CoreApp Versión
    A built on pure C# using .Net Core.
    Can be a candidate for CoreRT on Win-x64 and Linux-x64 platforms

---
+++ SfPack.Dotnet/SfpEntry.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SfPack.Dotnet
+{
+    /// <summary>
+    ///  Generic struct of sfp file entry.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct SfpEntry
+    {
+        public Int32 Magic { get; private set; }
+        public Int64 NameOffset { get; private set; }
+        public Int32 Unknown1 { get; private set; }
+        public Int64 ParentOffset { get; private set; }
+        public Int32 IsDir { get; private set; }
+        public Int64 FileLength { get; private set; }
+        public Int64 ModifiedTime { get; private set; }
+        public Int64 CreatedTime { get; private set; }
+        public Int64 Unknown2 { get; private set; }
+        public Int64 Unknown3 { get; private set; }
+        public Int64 StartOffset { get; private set; }
+        public Int32 DataLength { get; private set; }
+
+        /// <summary>
+        /// Builds a sfp entry from its raw bytes.
+        /// </summary>
+        /// <param name="data">Raw bytes.</param>
+        /// <returns>Sfp entry.</returns>
+        internal static unsafe SfpEntry CreateFromBytes(Byte[] data)
+        {
+            SfpEntry _directory;
+
+            if (data == null || data.Length != sizeof(SfpEntry))
+                throw new Exception("INVALID SFP ENTRY DATA");
+
+            fixed (Byte* map = &data[0])
+                _directory = *(SfpEntry*)map;
+
+            return _directory;
+        }
+        /// <summary>
+        /// Writes into the console human readable information about sfp file entry.
+        /// </summary>
+        internal void Print()
+        {
+            Console.WriteLine($"magic: {Magic}");
+            Console.WriteLine($"nameOffset: {NameOffset}");
+            Console.WriteLine($"unk1: {Unknown1}");
+            Console.WriteLine($"parentOffset: {ParentOffset}");
+            Console.WriteLine($"isDir: {IsDir}");
+            Console.WriteLine($"fileLength: {FileLength}");
+            Console.WriteLine($"modifiedTime: {ModifiedTime}");
+            Console.WriteLine($"createdTime: {CreatedTime}");
+            Console.WriteLine($"unk2: {Unknown2}");
+            Console.WriteLine($"unk3: {Unknown3}");
+            Console.WriteLine($"startOffset: {StartOffset}");
+            Console.WriteLine($"dataLength: {DataLength}");
+        }
+    }
+}
---
+++ SfPack.Dotnet/SfpHeader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SfPack.Dotnet
+{
+    /// <summary>
+    /// Generic struct of sfp file header.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct SfpHeader
+    {
+        public Int32 Magic { get; private set; }
+        public Int32 Version { get; private set; }
+        public Int64 Unknown1 { get; private set; }
+        public Int64 FirstEntryOffset { get; private set; }
+        public Int64 NameTableOffset { get; private set; }
+        public Int64 DataOffset { get; private set; }
+        public Int64 ArchiveSize { get; private set; }
+        public Int64 PackageLabelOffset { get; private set; }
+        public Int64 Unknown3 { get; private set; }
+
+        /// <summary>
+        /// Builds a sfp header from its raw bytes.
+        /// </summary>
+        /// <param name="data">Raw bytes.</param>
+        /// <returns>Sfp header.</returns>
+        internal static unsafe SfpHeader CreateFromBytes(Byte[] data)
+        {
+            SfpHeader _header;
+
+            if (data == null || data.Length != sizeof(SfpHeader))
+                throw new Exception("INVALID SFP HEADER DATA");
+
+            fixed (Byte* map = &data[0])
+                _header = *(SfpHeader*)map;
+
+            return _header;
+        }
+        /// <summary>
+        /// Writes into the console human readable information about sfp file header.
+        /// </summary>
+        internal void Print()
+        {
+            Console.WriteLine($"magic: {Magic}");
+            Console.WriteLine($"version: {Version}");
+            Console.WriteLine($"unk1: {Unknown1}");
+            Console.WriteLine($"firstDirOffset: {FirstEntryOffset}");
+            Console.WriteLine($"dataOffset: {DataOffset}");
+            Console.WriteLine($"archiveSize: {ArchiveSize}");
+            Console.WriteLine($"packageLabelOffset: {PackageLabelOffset}");
+            Console.WriteLine($"unk3: {Unknown3}");
+        }
+    }
+}
